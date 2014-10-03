79f33b3a30ceec93b62a4af8cdda85b361bcd2a6
Justin Gohde <justin.gohde@trinityschoolnyc.org>
2014-10-03 16:00:15 -0400

    Adding House Template for Testing

---
+++ House Template/javascript/menu.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+document.write('<h1><center><strong><a href="index.html">Computer Science I</a></strong></center></h1>');
+document.write('<p><center>');
+document.write('<a href="Static_House.html">Static House</a>'); 
+document.write(' ');
+document.write('<a href="Dynamic_House.html">Dynamic House</a>');
+document.write('</center></p>');
